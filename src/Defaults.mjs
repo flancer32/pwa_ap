64b8495e076a10a4d8db492e78f719eaf3ad2354
Alex Gusev <alex@flancer64.com>
2021-04-27 12:27:28 +0300

    CAM-35 i18n resources loading for plugins

--- src/Defaults.mjs
+++ src/Defaults.mjs
@@ -10,6 +10,8 @@ export default class Fl32_Ap_Defaults {
 
     /** @type {TeqFw_Core_App_Defaults} */
     MOD_CORE;
+    /** @type {TeqFw_Ui_Quasar_Defaults} */
+    MOD_QUASAR;
     /** @type {Fl32_Ap_User_Defaults} */
     MOD_USER;
     /** @type {TeqFw_Vue_Defaults} */
@@ -27,6 +29,7 @@ export default class Fl32_Ap_Defaults {
     constructor(spec) {
         // EXTRACT DEPS
         this.MOD_CORE = spec['TeqFw_Core_App_Defaults$']; // instance singleton
+        this.MOD_QUASAR = spec['TeqFw_Ui_Quasar_Defaults$']; // instance singleton
         this.MOD_USER = spec['Fl32_Ap_User_Defaults$']; // instance singleton
         this.MOD_VUE = spec['TeqFw_Vue_Defaults$']; // instance singleton
 
